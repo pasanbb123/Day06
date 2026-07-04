49f0428ad877b147e3ca659a77ac8a5f9cde5fc5
pasanbb123 <pasanthegrate@gmail.com>
2026-07-04 15:28:43 +0530

    saving in a array

--- js/app.js
+++ js/app.js
@@ -108,7 +108,22 @@
 let customerList = [];
 
 function btnCustomerOnAction() {
-    let txtName = document.getElementById("")
+    let txtName = document.getElementById("txtName").value;
+    let txtAddress = document.getElementById("txtAddress").value;
+    let txtAge = document.getElementById("txtAge").value;
+    let txtEmail = document.getElementById("txtEmail").value;
+    let txtSalary = document.getElementById("txtSalary").value;
 
+    let customer = {
+        name : txtName,
+        address : txtAddress,
+        age : txtAge,
+        email : txtEmail,
+        salary : txtSalary
+    }
+
+    customerList.push(customer);
+    console.log(customerList);
+    
 
 }
