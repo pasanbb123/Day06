69617de4035db5894e5170657b2d4b2f87f4bd8e
pasanbb123 <pasanthegrate@gmail.com>
2026-07-04 16:13:28 +0530

    adiing into a table

--- js/app.js
+++ js/app.js
@@ -107,23 +107,38 @@
 
 let customerList = [];
 
-function btnCustomerOnAction() {
+function btnAddCustomerOnAction(){
     let txtName = document.getElementById("txtName").value;
     let txtAddress = document.getElementById("txtAddress").value;
     let txtAge = document.getElementById("txtAge").value;
     let txtEmail = document.getElementById("txtEmail").value;
     let txtSalary = document.getElementById("txtSalary").value;
 
-    let customer = {
-        name : txtName,
-        address : txtAddress,
-        age : txtAge,
-        email : txtEmail,
-        salary : txtSalary
+    let customer={
+        name:txtName,
+        address:txtAddress,
+        age:txtAge,
+        email:txtEmail,
+        salary:txtSalary
     }
 
     customerList.push(customer);
-    console.log(customerList);
-    
 
+    console.log(customerList);
+      
+}
+
+function loadTable(){
+    let tblCustomers = document.getElementById("tblCustomers");
+
+    tblCustomers.innerHTML+=`<tr>
+            <td>Saman</td>
+            <td>Walana</td>
+            <td>15</td>
+            <td>saman@email.com</td>
+            <td>750000</td>
+        </tr>`
+
+    console.log(tblCustomers);
+    
 }
